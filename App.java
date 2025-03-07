9a37113817edb51931055fef55e745a23023620c
Catarina F. M. Castro <catfmcastro@gmail.com>
2025-03-07 15:21:26 -0300

    testing sqlite

---
+++ App.java
@@ -0,0 +1,44 @@
+package Musicas_BD_SQLite;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+        Connection connection = null;
+
+        try {
+            // todo mudar nome do bd
+            connection = DriverManager.getConnection("jdbc:sqlite:teste.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(20);
+
+            // ! apagar depois
+            statement.executeUpdate("DROP TABLE IF EXISTS terminalroot");
+            statement.executeUpdate("CREATE TABLE terminalroot (id INTEGER, name STRING)");
+            statement.executeUpdate("INSERT INTO terminalroot VALUES(1, 'TESTE Marcos Oliveira')");
+            statement.executeUpdate("INSERT INTO terminalroot VALUES(2, 'TESTE James Gosling')");
+            ResultSet rs = statement.executeQuery("SELECT * FROM terminalroot");
+            while (rs.next()) {
+                // Ler os dados inseridos
+                System.out.println("NOME DO CARA  : " + rs.getString("name"));
+                System.out.println("IDENTIFICAÇÃO : " + rs.getInt("id"));
+            }
+            // ! apagar depois
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // Falhou também para fechar o arquivo
+                System.err.println(e.getMessage());
+            }
+        }
+
+    }
+}
